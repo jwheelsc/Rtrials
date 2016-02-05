294d7294850b4fd57cceefd17b2fe3a864f9f51c
jwheelsc <jcrompto@sfu.ca>
2016-02-04 16:12:09 -0800

    end of day messing w/ r

--- plotFnc.R
+++ plotFnc.R
@@ -1,1 +1,10 @@
-##here you can read stuff in
+## this is really just a first attempt at R and git
+## the point of this script or function is to read in exp2 data, then
+## to plot the fractions of cations
+
+install.packages("gdata")
+library(gdata)
+
+setwd('D:/Code/Summer_2013_data/chem_data/exp2/Rtrials')
+data <- read.xls(file = "data/exp2_data.csv")
+data
